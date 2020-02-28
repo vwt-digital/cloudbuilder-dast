1f53009f452dab11ca6b1d18e6b6349c86a4edbd
MaxvanderWeide <max.vanderweide@student.hu.nl>
2020-02-28 11:41:13 +0100

    Added temp cors test DAT-2065

--- docker-dast.sh
+++ docker-dast.sh
@@ -5,17 +5,26 @@
 #
 set -eo pipefail
 if [ "$2" == "" ]; then
-    echo "This container expects two arguments: a domain name, and type"
+    echo "This container expects atleast two arguments: a domain name, and type"
     exit 1
 fi
 domain_name=$1
 type=$2
+resource=$3
 echo "--------"
 echo Starting verification of desired http plaintext behaviour...
 python /sec-helpers/verify-no-http/main.py ${domain_name}
 echo "--------"
 echo Starting HSTS max-age verification
 python /sec-helpers/verify-hsts/main.py ${domain_name}
+
+if [ "$3" != "" ]; then
+	echo "--------"
+	echo Starting CORS Policy verification
+	python /sec-helpers/verify-cors-policy ${domain_name} ${type} ${resource}
+else
+	echo "Skipping CORS"
+fi
 #echo "--------"
 #if [ "$type" == "frontend" ]; then
 #    echo Starting Content Security Policy validation...
